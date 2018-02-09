4dfb481ace81295f0d3a8643a436e6f32181783b
NJ-zero <821854810@qq.com>
2018-02-09 14:44:20 +0800

    增加发送邮件功能

--- runtest.py
+++ runtest.py
@@ -4,8 +4,21 @@
 import time,os
 import unittest
 import HTMLTestRunner
-from testcase.CmTest import Cm
+import smtplib
+import datetime
+from public.readConfig import Readconfig
+from public.Sendemail import Email
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.header import Header
+from email.mime.multipart import MIMEMultipart
 
+# from testcase.CmTest import Cm
+
+
+PATH = lambda p: os.path.abspath(
+        os.path.join(os.path.dirname(__file__), p)
+    )
 
 def testsuit():
     suite = unittest.TestSuite()
@@ -20,9 +33,6 @@ def testsuit():
     # runner.run(suite)
 
     now=time.strftime("%y-%m-%d-%H-%M-%S")
-    PATH = lambda p: os.path.abspath(
-        os.path.join(os.path.dirname(__file__), p)
-    )
     dirpath = PATH("./results/waiqin365-")
 
     filename=dirpath + now +'result.html'
@@ -32,5 +42,27 @@ def testsuit():
     runner.run(suite)
     fp.close()
 
+
+def send_email():
+    #定义发件箱
+    conf = Readconfig()
+    smtpsever = conf.getemailValue('smtpsever')
+    user = conf.getemailValue('user')
+    password = conf.getemailValue('password')
+    sender = conf.getemailValue('sender')
+    receiver = conf.getemailValue('receiver')
+
+    sendemail = Email()
+    msg=sendemail.email()
+
+    #发送邮件
+    smtp=smtplib.SMTP()
+    smtp.connect(smtpsever)
+    smtp.login(user,password)
+    smtp.sendmail(sender,receiver.split(','),msg.as_string())
+    smtp.quit()
+    print(u'邮件发送成功')
+
 if __name__ =="__main__":
-    testsuit()
+    # testsuit()
+    send_email()
